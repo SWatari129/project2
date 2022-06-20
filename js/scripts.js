675e0e3bc76cbe0dde9fe3ee935eb9399b8933d0
Spencer Watari <deanwatari@gmail.com>
2022-06-20 09:19:11 -0400

    adding array to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,39 @@
-function getResults() {
-  let name = "q1";
-  if (document.getElementById('radio1').checked == true) {
-    name = "JavaScript";
-  } else if (document.getElementById('radio2').checked == true) {
-    name = "C#";
-  } else if (document.getElementById('radio3').checked == true) {
-    name = "Ruby";
+function getResults(){
+  let choice1=0;
+  let choice2=0;
+  let choice3=0;
+
+  let choicesArray=document.getElementsByTagName('input');
+
+  for(let i=0; i<choicesArray.length;i++) {
+    if(choicesArray[i].checked) {
+      if(choicesArray[i].value === 'c1') {
+        choice1=choice1+1;
+      }
+      if(choicesArray[i].value === 'c2') {
+        choice2=choice2+1;
+      }
+      if(choicesArray[i].value === 'c3') {
+        choice3=choice3+1;
+      }
+    }  
   }
-  document.getElementById('outputBox').innerHTML = name;
+
+  let totalScore=Math.max(choice1,choice2,choice3);
+
+  let surveyResponse;
+  if(choice1==totalScore) {
+    surveyResponse="Ruby";
+  }
+  if(choice2==totalScore) {
+    surveyResponse="JavaScript";
+  }
+  if(choice3==totalScore) {
+    surveyResponse="C#";
+  
+  }
+  document.getElementById('output').innerHTML = "Your Programing Language is " + surveyResponse;
 }
-
-
-document.getElementById("submit").addEventListener("onclick"); {
-  preventDefault()
-};
+function resetForm() {
+  location.reload;
+}
