b4d8e9d424d84855cc0253b742144ad02d5e0720
Spencer Watari <deanwatari@gmail.com>
2022-06-26 10:56:47 -0400

    JS split Business & UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,53 @@
-//declaring variables
+window.addEventListener("load", function () {
+  const form = document.querySelector("#quiz");
+  const feedback = document.getElementById("feedback");
 
-const form = document.querySelector("#quiz");
-const feedback = document.querySelector("#quiz");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+    feedback.innerHTML = "";
 
-  const q1 = document.querySelector("input[name=q1]:checked").value;
-  const q2 = document.querySelector("input[name=q2]:checked").value;
-  const q3 = document.querySelector("input[name=q3]:checked").value;
-  const q4 = document.querySelector("input[name=q4]:checked").value;
-  const q5 = document.querySelector("input[name=q5]:checked").value;
+    const q1 = document.querySelector("input[name=q1]:checked").value;
+    const q2 = document.querySelector("input[name=q2]:checked").value;
+    const q3 = document.querySelector("input[name=q3]:checked").value;
+    const q4 = document.querySelector("input[name=q4]:checked").value;
+    const q5 = document.querySelector("input[name=q5]:checked").value;
 
-  function getResults() {
-    let choice1 = 0;
-    let choice2 = 0;
-    let choice3 = 0;
+    choicesArray = [q1, q2, q3, q4, q5];
 
-    for (let i = 0; i < choicesArray.length; i++) {
-      if (choicesArray.length > 0) {
-        if (choicesArray[i] === "c1") {
-          choice1++;
-        }
-        if (choicesArray[i] === "c2") {
-          choice2++;
-        }
-        if (choicesArray[i] === "c3") {
-          choice3++;
+    function getResults() {
+      let choice1 = 0;
+      let choice2 = 0;
+      let choice3 = 0;
+
+      for (let i = 0; i < choicesArray.length; i++) {
+        if (choicesArray.length > 0) {
+          if (choicesArray[i] === "c1") {
+            choice1++;
+          }
+          if (choicesArray[i] === "c2") {
+            choice2++;
+          }
+          if (choicesArray[i] === "c3") {
+            choice3++;
+          }
         }
       }
+
+      if (choice1 > choice2 && choice3) {
+        return "Ruby";
+      } else if (choice2 > choice1 && choice3) {
+        return "JavaScript";
+      } else {
+        return "C#";
+      }
     }
-    return [choice1, choice2, choice3];
-  }
 
-  let surveyResponse;
-  if (choice1 > choice2 && choice3) {
-    surveyResponse = "Ruby";
-  } else if (choice2 > choice1 && choice3) {
-    surveyResponse = "JavaScript";
-  } else {
-    surveyResponse = "C#";
-  }
-
-  let answer = getResults();
-  feedback.innerHTML += `Your Programing Language is ${answer}`;
+    let answer = getResults();
+    feedback.innerHTML += `Your Programing Language is ${answer}`;
+  });
 });
+
 
     
   // let totalScore=Math.max(choice1,choice2,choice3);
