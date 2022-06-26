750c376de97dd7c5faf975b2a9ae6abc65855d8b
Spencer Watari <deanwatari@gmail.com>
2022-06-26 01:43:45 -0400

    adding doc.getElememntById to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,24 @@
 //declaring variables
-  // let choicesArray=document.getElementsByTagName('input');
 
-  // let form = document.querySelector("#quiz")
-  // let submit  = document.querySelector("#submit")
-  // let reset  = document.querySelector("#reset")
 
-  // console.log(form)
 
-window.addEventListener('load', function() {
+function getResults(){
+  let choice1=0;
+  let choice2=0;
+  let choice3=0;
+
   let choicesArray=document.getElementsByTagName('input');
-  let form = document.querySelector("#quiz")
-  let submit  = document.querySelector("#submit")
-  let reset  = document.querySelector("#reset")
 
-}) 
+let q1 = document.getElementById('q1')
+let q2 = document.getElementById('q2')
+let q3 = document.getElementById('q3')
+let q4 = document.getElementById('q4')
+let q5= document.getElementById('q5')
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
+console.log(q1, q2, q3, q4, q5)
 
-    let choice1=0;
-    let choice2=0;
-    let choice3=0;
+
+    
 
   for(let i=0; i<choicesArray.length;i++) {
     if(choicesArray[i].checked) {
@@ -35,14 +33,6 @@ form.addEventListener('submit', e => {
       }
     }  
   }
-})
-
-form.addEventListener('reset', e => {
-  choice1=0;
-  choice2=0;
-  choice3=0;
-})
-
   let totalScore=Math.max(choice1,choice2,choice3);
 
     let surveyResponse;
@@ -56,5 +46,8 @@ form.addEventListener('reset', e => {
       surveyResponse="C#";
     }
     alert("Your Programing Language is " + surveyResponse);
-  
-  
+
+    let output = document.getElementById('#output')
+
+    output.textContent = "Hello!"
+  }
